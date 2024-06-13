9eb261352108b815761e0bdba86a1b629dba3230
Lasandri silva <120217797+Lasandri@users.noreply.github.com>
2024-06-13 11:54:33 +0530

    new updated

--- basket.php
+++ basket.php
@@ -8,62 +8,122 @@ echo "<body>";
 include ("headfile.html");                                                          //include header layout file 
 echo "<h4>".$pagename."</h4>";                                                      //display name of the page on the web page
 
-    if(isset($_POST['prodid'])){
-        $newprodid = isset($_POST['prodid']) ? $_POST['prodid'] : 0;
+    // if(isset($_POST['prodid'])){
+    //     $newprodid = isset($_POST['prodid']) ? $_POST['prodid'] : 0;
 
-                                            // Capture the required quantity of selected product using the POST method and $_POST superglobal variable 
-                                            // and store it in a new local variable called $reququantity
-        $reququantity = isset($_POST['p_quantity']) ? $_POST['p_quantity'] : 0;
+    //                                         // Capture the required quantity of selected product using the POST method and $_POST superglobal variable 
+    //                                         // and store it in a new local variable called $reququantity
+    //     $reququantity = isset($_POST['p_quantity']) ? $_POST['p_quantity'] : 0;
 
-                                            // Display id of selected product
-        echo "<p>Selected product ID: ".$newprodid."</p>";
+    //                                         // Display id of selected product
+    //     echo "<p>Selected product ID: ".$newprodid."</p>";
 
-        echo "<p>Quantity of selected product: ".$reququantity."</p>";                      // Display quantity of selected product
-
-
-        $_SESSION['basket'][$newprodid] = $reququantity;        // Update the session array with the product id and required quantity
+    //     echo "<p>Quantity of selected product: ".$reququantity."</p>";                      // Display quantity of selected product
 
 
-        echo "<p>1 item added</p>";        // Display message indicating that the item is added to the basket
-
-    }  else{
-        echo "<p> Basket unchanged</p>";
-    }  
+    //     $_SESSION['basket'][$newprodid] = $reququantity;        // Update the session array with the product id and required quantity
 
 
-    $total = 0;
+    //     echo "<p>1 item added</p>";        // Display message indicating that the item is added to the basket
 
-    if(isset($_SESSION['basket'])){
-        echo '<table>';
-        echo "<tr><th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr>";
+    // }  else{
+    //     if(isset($_POST['del_prodid'])) {
+    //         // Capture the posted product ID
+    //         $delprodid = $_POST['del_prodid'];
+            
+    //         // Unset the cell of the session for the posted product ID
+    //         unset($_SESSION['basket'][$delprodid]);
+            
+    //         // Display "1 item removed from the basket" message
+    //         echo "<p>1 item removed from the basket</p>";
         
+    // }  
 
-        foreach ($_SESSION['basket'] as $index => $value) {
+
+    //     $total = 0;
+    //     echo '<form method="post" action="basket.php">';
+    //     echo '<table id="baskettable">';
+    //     echo "<tr><th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th><th>Remove Product</th></tr>";
+        
+            $total = 0;
+        if (isset($_POST['prodid']))
+        {
+            $newprodid =$_POST['prodid'];
+            $reququantity =$_POST['p_quantity'];
             
-            $SQL = "SELECT prodName, prodPrice FROM Product WHERE prodId=$index";
+            echo "ID of selected Product:".$newprodid;
+            echo "Qty of selected Product:".$reququantity;
             
-            $exeSQL = mysqli_query($conn, $SQL) or die (mysqli_error($conn));
-            $arrayp = mysqli_fetch_array($exeSQL);
-    
-            // Create a new HTML table row
-            echo "<tr>";
-            echo "<td>".$arrayp['prodName']."</td>";
-            echo "<td>".$arrayp['prodPrice']."</td>";
-            echo "<td class = 'baskettable'>".$value."</td>";  
-             $subtotal = $arrayp['prodPrice'] * $value; 
-            echo "<td '>".$subtotal."</td>";
+            //create a new cell in the basket session array. Index this cell with the new product id.
+        //Inside the cell store the required product quantity
+            $_SESSION['basket'][$newprodid]=$reququantity;
+        }
 
-            echo "</tr>";
-
-            $total=$total+$subtotal;}}//else display empty basket message
-            else
-            {echo "<p>Empty basket";}// Display total
-            echo "<tr>";
-            echo "<td colspan=3>TOTAL</td>";
-            echo "<td>&pound".number_format($total,2)."</td>";
-            echo "</tr>";echo "</table>";
+        else
+        {
+            echo "<p>Current basket unchanged";
+        }
+            echo "<table border='1' style='width:50%'>
+            <tr>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>SubTotal</th>
+                <th>Remove Product</th>
+            </tr>";
 
 
-include("footfile.html");                                                           //include head layout
-echo "</body>";
-?>
+
+            if(isset($_SESSION['basket'])){
+
+                foreach($_SESSION['basket'] as $key => $value)
+                {	
+                    $SQL="select prodId,prodName,prodPrice from product where prodId = '".$key."';";
+                    //Create a new variable containing the execution of the SQL query i.e. select the records or get out
+                    $exeSQL=mysqli_query($conn,$SQL) or die (mysqli_error());
+                    $arrayprod=mysqli_fetch_array($exeSQL);
+                    echo "<tr>
+                    <td>".$arrayprod['prodName']."</td>
+                    <td>".$arrayprod['prodPrice']."</td>
+                    <td>".$value."</td>
+                    <td> Rs ".$arrayprod['prodPrice']*$value."</td>";
+                    $total = $total+($arrayprod['prodPrice']*$value);
+            echo "<form action=basket.php method=post>";
+            echo "<td>";
+            echo "<input type=submit value='Remove'>";
+            echo "</td>";
+            echo "<input type=hidden name=del_prodid value=".$arrayprod['prodId'].">";
+            echo "</form>";
+                }
+            
+                    }
+            else{
+                echo "Empty Basket";
+                }
+                    echo "<tr><td colspan='4'>Total</td><td>Rs ".$total."</td></tr></table>";
+                    echo "<a href='clearBasket.php'>Clear the basket</a>";
+                    echo "<br><br>";
+                    echo "New HomeTeq Customers";
+                    echo "<a href='register.php'> Register</a>";
+                    echo "<br>";
+                    echo "Returning HomeTeq Customers";
+                    echo "<a href='login.php'> Login</a>";
+                    
+                    
+                    
+                    if (isset($_POST['del_prodid']))
+                    {
+                    //capture the posted product id and assign it to a local variable $delprodid
+                    $delprodid=$_POST['del_prodid'];
+                    //unset the cell of the session for this posted product id variable
+                    unset ($_SESSION['basket'][$delprodid]);
+                    //display a "1 item removed from the basket" message
+                    header("Refresh:0");
+                    echo "<p>1 item removed";
+                    }
+                    
+            
+            include("footfile.html");
+            //include head layout
+            echo "</body>";
+            ?>
---
+++ clearbasket.php
@@ -0,0 +1,17 @@
+<?php
+session_start(); // Start the session
+include("db.php");
+$pagename="Clear Smart Basket";                                                               //Create and populate a variable called $pagename
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";                   //Call in stylesheet
+echo "<title>".$pagename."</title>";                                                //display name of the page as window title
+echo "<body>";
+include ("headfile.html");                                                          //include header layout file 
+echo "<h4>".$pagename."</h4>";                                                      //display name of the page on the web page
+//display random text
+
+unset($_SESSION['basket']); // Clear the session array
+echo "<p>Your basket has been cleared</p>";
+
+include("footfile.html");                                                           //include head layout
+echo "</body>";
+?>
